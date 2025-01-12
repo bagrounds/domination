388f8a2c05f052657b4bb2831529eac96aafa7fd
Bryan Grounds <bagrounds@gmail.com>
2025-01-12 01:02:22 +0000

    - improve test code readability variable - add a lot of explanatory comments - add a few new tests - improve test output to be more readable - v0.33.3

--- src/Data/Stack/Machine.purs
+++ src/Data/Stack/Machine.purs
@@ -19,39 +19,37 @@ data StackMachineChoice i o
     , program :: Program i o
     }
 
-ex :: Effect Unit
-ex = do
-  let
-    -- A stack program operates on a value (X) and a stack.
-    -- We can apply functions to X,
-    --        push values on the stack,
-    --    and pop values off the stack.
-    -- The program itself is a list of stack operatons.
-    i = 1
-    p = Program
-      -- (1 nil)         start with the input (X) and an empty stack (nil)
-      ( cons (id      :: forall z. Operation (Tuple  Int            z ) (Tuple  Int            z ))
-      -- (1 nil)         apply the identity function to X
-      $ cons (tuple 7 :: forall z. Operation (Tuple  Int            z ) (Tuple (Tuple Int Int) z ))
-      -- ((7 1) nil)     pair 7 with X
-      $ cons (push    :: forall z. Operation (Tuple (Tuple Int Int) z ) (Tuple  Int (Tuple Int z)))
-      -- (7 (1 nil))     push (snd X) onto the stack
-      $ cons (addN 4  :: forall z. Operation (Tuple  Int            z ) (Tuple  Int            z ))
-      -- (11 (1 nil))    add 4 to X
-      $ cons (subN 1  :: forall z. Operation (Tuple  Int            z ) (Tuple  Int            z ))
-      -- (10 (1 nil))    subtract 1 from X
-      $ cons (pop     :: forall z. Operation (Tuple  Int (Tuple Int z)) (Tuple (Tuple Int Int) z ))
-      -- ((10 1) nil)    pop the top element off the stack and pair X with it
-      $ cons (add     :: forall z. Operation (Tuple (Tuple Int Int) z ) (Tuple Int             z ))
-      -- (11 nil)        add (fst X) and (snd X)
-      $ cons (mulN 3  :: forall z. Operation (Tuple  Int            z ) (Tuple Int             z ))
-      -- (33 nil)        multiply X by 3
-      $ cons (divN 11 :: forall z. Operation (Tuple  Int            z ) (Tuple Int             z ))
-      -- (3 nil)         divide X by 11
-      $ nil
-      )
-    b = evalProgram p i
-  Console.log $ show b
+exampleStackMachineComputation :: Int -> Int
+exampleStackMachineComputation input = let
+  -- A stack program operates on a value (X) and a stack.
+  -- We can apply functions to X,
+  --        push values on the stack,
+  --    and pop values off the stack.
+  -- The program itself is a list of stack operatons.
+  program = Program
+    -- (1 nil)         start with the input (X) and an empty stack (nil)
+    ( cons (id      :: forall z. Operation (Tuple  Int            z ) (Tuple  Int            z ))
+    -- (1 nil)         apply the identity function to X
+    $ cons (tuple 7 :: forall z. Operation (Tuple  Int            z ) (Tuple (Tuple Int Int) z ))
+    -- ((7 1) nil)     pair 7 with X
+    $ cons (push    :: forall z. Operation (Tuple (Tuple Int Int) z ) (Tuple  Int (Tuple Int z)))
+    -- (7 (1 nil))     push (snd X) onto the stack
+    $ cons (addN 4  :: forall z. Operation (Tuple  Int            z ) (Tuple  Int            z ))
+    -- (11 (1 nil))    add 4 to X
+    $ cons (subN 1  :: forall z. Operation (Tuple  Int            z ) (Tuple  Int            z ))
+    -- (10 (1 nil))    subtract 1 from X
+    $ cons (pop     :: forall z. Operation (Tuple  Int (Tuple Int z)) (Tuple (Tuple Int Int) z ))
+    -- ((10 1) nil)    pop the top element off the stack and pair X with it
+    $ cons (add     :: forall z. Operation (Tuple (Tuple Int Int) z ) (Tuple Int             z ))
+    -- (11 nil)        add (fst X) and (snd X)
+    $ cons (mulN 3  :: forall z. Operation (Tuple  Int            z ) (Tuple Int             z ))
+    -- (33 nil)        multiply X by 3
+    $ cons (divN 11 :: forall z. Operation (Tuple  Int            z ) (Tuple Int             z ))
+    -- (3 nil)         divide X by 11
+    $ nil
+    )
+  in
+    evalProgram program input
 
 exCellar :: Effect Unit
 exCellar = do
--- test/Main.purs
+++ test/Main.purs
@@ -1,3 +1,45 @@
+{- |
+This module contains test cases for a card game's network functionality, focusing on data serialization.
+
+Key Testing Areas:
+1. Binary serialization for network transmission
+2. Game state consistency checks
+3. Automated gameplay validation
+4. Data conversion integrity
+
+Technical Concepts:
+* Effect: Represents side-effecting computations (IO, networking, etc).
+  Learn more: https://pursuit.purescript.org/packages/purescript-effect
+* Type Classes:
+  - Eq: Value equality comparison
+  - EncodeArrayBuffer/DecodeArrayBuffer: Binary format conversion
+  - WireCodec: Network protocol serialization
+
+Key concepts for non-functional programmers:
+* Effect: Represents computations that interact with the outside world (like logging or network calls).
+  Similar to Promise/async in JavaScript.
+
+* Type Classes (indicated by 'class' in type signatures):
+  - Eq: Allows comparing values for equality (like implementing .equals() in Java)
+  - EncodeArrayBuffer/DecodeArrayBuffer: For converting data to/from binary format
+  - WireCodec: For network transmission serialization
+
+* Functional Programming Patterns:
+  - Isomorphism: A pair of functions that can convert between two types without losing information
+    (like a perfect two-way conversion between JSON and an object)
+  - Lens: A way to access and modify nested data structures (similar to lodash.get/set)
+  - traverse: Like map/forEach but handles nested effects properly
+  - Maybe: Represents optional values (like Optional in Java/Swift)
+  - Either: Represents success/failure results (Left = error, Right = success)
+
+Testing Strategy:
+Tests verify that game state can be:
+1. Serialized to binary format
+2. Transmitted over network
+3. Deserialized without data loss
+4. Maintain game rule consistency
+-}
+
 module Test.Main where
 
 import Prelude
@@ -9,94 +51,125 @@ import Data.Lens.Getter (view)
 import Data.Lens.Iso (Iso')
 import Data.Lens.Prism (review)
 import Data.Maybe (Maybe(..))
+import Data.Stack.Machine as Machine
 import Data.Traversable (traverse_)
 import Domination.Capability.Random (runRandomM)
 import Domination.Capability.WireCodec (class WireCodec, readWire, writeWire)
 import Domination.Data.Cards as Cards
-import Domination.Data.Game.Engine (makeAutoPlay) as Dom
 import Domination.Data.Game as Game
+import Domination.Data.Game.Engine (makeAutoPlay) as Dom
 import Domination.Data.Play (Play(..))
 import Domination.Data.Wire.Game (_toWire) as Dom
-import Data.Stack.Machine (ex)
+import Domination.Data.Wire.Play (_toWire) as Play
 import Effect (Effect)
-import Test.QuickCheck (quickCheck')
+import Effect.Console (log)
+import Test.QuickCheck (Result(..), assertEquals)
 
+-- | Entry point for the test suite
 main :: Effect Unit
 main = do
-  quickCheck' 1 `traverse_` examples
-  ex
+  run_test test_expected_stack_machine_value
+  traverse_ run_test test_cases
 
--- can't run this test outside of the browser due to FFI.js
--- using browser specific APIs (window) and libraries (LZString)
---  let wireWitchChoice = view _choiceToWire Cards.witchChoice
---  b <- runWireCodecM $ write_read wireWitchChoice
---  quickCheck' 1 b
+-- | Verifies that stack machine produces expected value
+test_expected_stack_machine_value :: { name :: String, test :: Result }
+test_expected_stack_machine_value =
+  { name: "Stack machine should output 3"
+  , test: assertEquals (Machine.exampleStackMachineComputation 1) 3
+  }
 
--- properties
+-- | Runs a single test and logs its results
+run_test :: { name :: String, test :: Result } -> Effect Unit
+run_test test_case = do
+  log $ "Testing: " <> test_case.name
+  log $ show test_case.test
 
-encode_decode
+-- | Test suite covering core game functionality
+test_cases :: Array { name :: String, test :: Result }
+test_cases = 
+  (map
+    (\player_count -> 
+      { name: "Wire serialization roundtrip with " <> show player_count <> " player game"
+      , test: test_game_wire_serialization player_count
+      })
+    (1 .. 10))
+  <>
+  [ { name: "Game wire format isomorphism"
+    , test: test_isomorphism Dom._toWire (Game.new 2 Cards.cardMap true)
+    }
+  , { name: "Play wire format isomorphism"
+    , test: test_isomorphism Play._toWire (NewGame { playerCount: 2, supply: Cards.cardMap, longGame: true })
+    }
+  ]
+
+-- | Verifies binary serialization integrity
+encode_and_decode
   :: forall a
   . EncodeArrayBuffer a
   => DecodeArrayBuffer a
   => DynamicByteLength a
   => Eq a
+  => Show a
   => a
-  -> Effect Boolean
-encode_decode x = do
-  ab <- encodeArrayBuffer x
-  mbX <- decodeArrayBuffer ab
-  pure case mbX of
-    Nothing -> false
-    Just x' -> x == x'
+  -> Effect Result
+encode_and_decode original_value = do
+  binary_buffer <- encodeArrayBuffer original_value
+  deserialized_value <- decodeArrayBuffer binary_buffer
+  pure $ assertEquals deserialized_value $ Just original_value
 
-write_read
+-- | Tests network transmission safety by simulating a complete
+-- | wire format serialization cycle. Essential for verifying that
+-- | game state can be safely shared between players.
+test_wire_roundtrip
   :: forall m a
   . WireCodec m
   => EncodeArrayBuffer a
   => DynamicByteLength a
   => DecodeArrayBuffer a
   => Eq a
+  => Show a
   => a
-  -> m Boolean
-write_read x = do
-  eS <- writeWire x
-  case eS of
-    Left _ -> pure false
-    Right s -> do
-      eX' <- readWire s
-      case eX' of
-        Left _ -> pure false
-        Right x' -> pure $ x' == x
+  -> m Result
+test_wire_roundtrip value_to_transmit = do
+  wire_format <- writeWire value_to_transmit
+  case wire_format of
+    Left err -> pure $ Failed $ "Failed to serialize: " <> show err
+    Right wire_data -> do
+      restored_value <- readWire wire_data
+      case restored_value of
+        Left err -> pure $ Failed $ "Failed to deserialize: " <> show err
+        Right received_value -> pure $ assertEquals received_value value_to_transmit
 
-game_wire_iso :: Int -> Boolean
-game_wire_iso n = iso_prop Dom._toWire (Game.new n Cards.cardMap true)
+-- | Validates game state consistency during network transmission
+test_game_wire_serialization :: Int -> Result
+test_game_wire_serialization player_count =
+  let initial_game = Game.new player_count Cards.cardMap true
+      serialized = view Dom._toWire initial_game
+      deserialized = review Dom._toWire serialized
+  in assertEquals deserialized initial_game
 
--- examples
-
-examples :: Array Boolean
-examples = game_wire_iso <$> (1 .. 10)
-
-exampleGame :: Effect Boolean
-exampleGame = do
-  let g0 = Game.new 1 Cards.cardMap true
-  g1 <- runRandomM
+-- | Comprehensive game turn simulation
+example_game_simulation :: Effect Result
+example_game_simulation = do
+  let initial_game = Game.new 1 Cards.cardMap true
+  game_state_after_turn <- runRandomM
     $ Dom.makeAutoPlay
       (NewGame { playerCount: 1, supply: Cards.cardMap, longGame: true })
-      g0
-  pure case g1 of
-    Left _ -> false
-    Right _ -> true
+      initial_game
+  pure case game_state_after_turn of
+    Left error -> Failed $ "Auto play failed: " <> show error
+    Right final_state -> assertEquals final_state final_state
 
--- helpers
-
-iso_prop
+-- | Tests data conversion integrity using lenses
+test_isomorphism
   :: forall a b
   . Eq a
+  => Show a
   => (Iso' a b)
   -> a
-  -> Boolean
-iso_prop iso before = let
-  mapped = view iso before
-  after = review iso mapped
-  in before == after
+  -> Result
+test_isomorphism conversion_lens original_value =
+  let converted = view conversion_lens original_value
+      restored = review conversion_lens converted
+  in assertEquals restored original_value
 
