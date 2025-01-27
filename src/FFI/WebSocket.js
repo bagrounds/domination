0a9b2e3e636f13bbdacdae93f550b31b52dbbd20
Bryan Grounds <bagrounds@gmail.com>
2025-01-26 17:18:08 -0800

    copilot implements FFI according to the new module and referencing bugout FFI

--- src/FFI/WebSocket.js
+++ src/FFI/WebSocket.js
@@ -1,20 +1,71 @@
 'use strict'
 
-exports.createImpl = url => () => {
-  const ws = new WebSocket(url)
-  return ws
-}
+const log = level => (...args) => console[level]('WebSocket FFI: ', ...args)
+const logInfo = (...args) => log('log')(...args)
+const logError = (...args) => log('error')(...args)
 
-exports.onMessageImpl = ws => callback => () => {
-  ws.onmessage = event => {
-    callback(event.data)()
+const broadcastEvent = messageTarget => event => {
+  const eventTarget = document.querySelector('#' + messageTarget)
+  if (eventTarget) {
+    eventTarget.dispatchEvent(new CustomEvent('purescript', { detail: event }))
+  } else {
+    logError(`#${messageTarget} undefined, cannot dispatch event: `, event)
   }
 }
 
-exports.sendImpl = ws => data => () => {
-  ws.send(data)
+exports.detail = ({ detail }) => detail
+
+exports.showWebSocket = ws => `WebSocket(${ws.url})`
+
+exports.makeWebSocketFFI = left => right => connections => seen =>
+  remoteMessageTarget => localMessageTarget => roomCode => announce => callback => () => {
+    try {
+      const ws = new WebSocket(`ws://192.168.254.72:8081`)
+      ws.address = crypto.randomUUID()
+
+      const shutdown = (event) => {
+        try {
+          ws.close()
+          logInfo(`closed websocket on '${event.type}'`)
+        } catch (e) {
+          logInfo(`failed to close websocket on '${event.type}'`)
+        }
+      }
+
+      window.addEventListener("unload", shutdown)
+      window.addEventListener("beforeunload", shutdown)
+
+      ws.onopen = () => {
+        logInfo('WebSocket connected')
+        broadcastEvent(localMessageTarget)(connections(1))
+        callback(right(ws))()
+      }
+
+      ws.onclose = () => {
+        logInfo('WebSocket disconnected')
+        broadcastEvent(localMessageTarget)(connections(0))
+      }
+
+      ws.onmessage = event => {
+        logInfo('Received message:', event.data)
+        broadcastEvent(remoteMessageTarget)(event.data)
+      }
+
+      ws.onerror = error => {
+        logError('WebSocket error:', error)
+        callback(left(error))()
+      }
+
+      return () => shutdown({ type: 'cancel' })
+    } catch (error) {
+      callback(left(error))()
+      return () => {}
+    }
 }
 
-exports.closeImpl = ws => () => {
-  ws.close()
+exports.send = ws => message => () => {
+  logInfo("sent message: ", message)
+  ws.send(message)
 }
+
+exports.address = ws => () => ws.address
